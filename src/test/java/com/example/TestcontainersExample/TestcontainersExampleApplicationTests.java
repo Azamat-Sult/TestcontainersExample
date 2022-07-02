5966493d8f9fe79b59c3dab88d223aa81bc297b6
AzamaT_S <asultangareev@gmail.com>
2022-07-02 21:56:22 +0300

    Init

---
+++ src/test/java/com/example/TestcontainersExample/TestcontainersExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestcontainersExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
